cbfcd94c3ff05d428ffd743ff48ed7cedde9e1ce
lmthirds <lmthirds@outlook.com>
2026-09-20 17:04:24 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -59,7 +59,7 @@ public class AppService {
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "17157");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
-    private static final String ANYTLS_PORT = env("ANYTLS_PORT", "17157");
+    private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "saas.sin.fan");
     private static final int CFPORT = envInt("CFPORT", 443);
