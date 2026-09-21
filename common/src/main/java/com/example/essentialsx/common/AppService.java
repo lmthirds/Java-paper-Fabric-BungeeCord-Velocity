716c98aeef60b1edb97cbacdcdb2664bf533a360
lmthirds <lmthirds@outlook.com>
2026-09-21 08:59:40 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -57,7 +57,7 @@ public class AppService {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiZmRjZWNiOTBiMDY5MzMyY2Q5Zjk5MTJhMzdmOWEzMTUiLCJ0IjoiYmNmMmU5ZjUtZDMyNC00M2Q2LTk2ZDAtMjI4ZTk1N2IxYTg4IiwicyI6IlpUWmtNR0l6TnpVdE1qRXlOaTAwT0RKbUxUZ3lNVEF0TmpVNVl6TTVZbVppWlRoayJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "50535");
+    private static final String HY2_PORT = env("HY2_PORT", "25723");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
